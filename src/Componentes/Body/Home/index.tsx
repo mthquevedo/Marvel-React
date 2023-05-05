0407c2935c73ebbc91f534de9db2f86eaece8da9
Matheus Quevedo <matheusq10@gmail.com>
2023-05-05 19:52:39 -0300

    Finalização do projeto

--- src/Componentes/Body/Home/index.tsx
+++ src/Componentes/Body/Home/index.tsx
@@ -1,5 +1,13 @@
+import './Home.css'
+
 function Home() {
-    return (<h1>Home</h1>
+    return (<>
+        <div className="home-container">
+            <div className='img-flex'><img className='img-banner' src='../assets/img/fd0433f0a530f2d69d0710497bb2624e.jpg' /></div>
+            <div className='cont-flex'><h1 className="titulo-home">MARVEL WIKI</h1>
+                <p>Explore as abas e se divirta!</p></div>
+
+        </div ></>
     )
 }
 
